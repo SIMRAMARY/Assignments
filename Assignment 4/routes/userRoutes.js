0c9d137bf908e0c9758dbe7e10b586e57c61f159
SIMRAMARY P <128148650+SIMRAMARY@users.noreply.github.com>
2025-11-15 14:34:21 +0530

    Update userRoutes.js

--- Assignment 4/routes/userRoutes.js
+++ Assignment 4/routes/userRoutes.js
@@ -5,6 +5,7 @@ const UserApiRouter = express.Router()
 UserApiRouter.post("/adduser",MyController.AddUser)
 UserApiRouter.put("/edituser/:id",MyController.UpdateUser)
 UserApiRouter.get("/getuser",MyController.GetUser)
-UserApiRouter.delete("/deluser",MyController.deleteUser)
+UserApiRouter.delete("/deluser/:id",MyController.deleteUser)
+
 
 module.exports= UserApiRouter
